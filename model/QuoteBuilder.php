8cc49ec2eb5e8f133291797dae31672e5fb4e28a
iLexN <ilex.job@gmail.com>
2016-01-08 10:52:50 +0800

    quote -> break down 3 class

--- model/QuoteBuilder.php
+++ model/QuoteBuilder.php
@@ -2,6 +2,8 @@
 
 namespace Ksi;
 
+use Ksi\QuotePush;
+use Ksi\QuoteDownload;
 use ORM as ORM;
 
 /**
@@ -25,8 +27,7 @@ class QuoteBuilder
                 find_one($ar['id']);
 
         if ($oneQuoteOrm->status === '0') {
-            $q = new Quote();
-            $q->setOrmObjFromLocal($oneQuoteOrm);
+            $q = new QuotePush($oneQuoteOrm);
             $q->proccessDataToYellowSheet($ar['sale']);
 
             return [
@@ -87,7 +88,7 @@ class QuoteBuilder
         $manyQuote = $manyQuoteOrm->find_many();
 
         $arQuoteIdAr = [];
-        $q = new Quote();
+        $q = new QuoteDownload();
         foreach ($manyQuote as $quoteOrm) {
             $q->processDownload($quoteOrm);
             $arQuoteIdAr[] = $quoteOrm->id;
---
+++ model/QuoteDownload.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Ksi;
+
+use ORM as ORM;
+
+/**
+ * Quote Model.
+ */
+class QuoteDownload
+{
+    /**
+     * @var object ORM-object
+     */
+    private $ormObjFromSource;
+
+    /**
+     * @var object ORM-Object
+     */
+    public $ormObjFromLocal;
+
+    /**
+     * nothing.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Flow for process Download -> save , update , find dup.
+     *
+     * @param ORM $ormQuote orm-object
+     */
+    public function processDownload($ormQuote)
+    {
+        $this->ormObjFromSource = $ormQuote;
+        $this->saveDownload();
+        $this->updateSourceDownload();
+    }
+
+    /**
+     * set the ORM object for Locat DB.
+     *
+     * @param object $ormObj orm-object
+     */
+    public function setOrmObjFromLocal($ormObj)
+    {
+        $this->ormObjFromLocal = $ormObj;
+    }
+
+    /**
+     * Save the download data from source to local.
+     */
+    private function saveDownload()
+    {
+        $this->ormObjFromLocal = ORM::for_table('motor_quote', 'local')->create();
+        $this->ormObjFromLocal->set($this->ormObjFromSource->as_array());
+        $this->ormObjFromLocal->save();
+    }
+
+    /**
+     * update the source which will no download again
+     * this process after saveDownload().
+     */
+    private function updateSourceDownload()
+    {
+        $this->ormObjFromSource->download = 1;
+
+        $this->ormObjFromSource->hkid_1 = '';
+        $this->ormObjFromSource->hkid_2 = '';
+        $this->ormObjFromSource->hkid_3 = '';
+
+        $this->ormObjFromSource->hkid_1_2 = '';
+        $this->ormObjFromSource->hkid_2_2 = '';
+        $this->ormObjFromSource->hkid_3_2 = '';
+
+        $this->ormObjFromSource->dob = '';
+        $this->ormObjFromSource->dob2 = '';
+
+        $this->ormObjFromSource->save();
+    }
+}
