61afc8e82aec189f77c1756dd28f951beab8408d
iLexN <ilex.job@gmail.com>
2016-02-03 17:10:43 +0800

    update status to ksi

--- 20140903/model/Quote.php
+++ 20140903/model/Quote.php
@@ -50,7 +50,7 @@ class Quote
         $this->updateSourceDownload();
         //$this->findDuplicate();
     }
-   
+
     /**
      * set the ORM object for Locat DB
      *
@@ -70,7 +70,7 @@ class Quote
         $this->ormObjFromLocal->set($this->ormObjFromSource->as_array());
         $this->ormObjFromLocal->save();
     }
-    
+
     /**
      * Save the download data from source to local
      */
@@ -89,18 +89,18 @@ class Quote
     private function updateSourceDownload()
     {
         $this->ormObjFromSource->download = 1;
-        
+
         $this->ormObjFromSource->hkid_1 = '';
         $this->ormObjFromSource->hkid_2 = '';
         $this->ormObjFromSource->hkid_3 = '';
-        
+
         $this->ormObjFromSource->hkid_1_2 = '';
         $this->ormObjFromSource->hkid_2_2 = '';
         $this->ormObjFromSource->hkid_3_2 = '';
-        
+
         $this->ormObjFromSource->dob = '';
         $this->ormObjFromSource->dob2 = '';
-        
+
         $this->ormObjFromSource->save();
     }
 
@@ -131,7 +131,7 @@ class Quote
 
         return 'No';
     }
-    
+
     /**
      * layout use function.
      *
@@ -144,7 +144,7 @@ class Quote
         }
         return ;
     }
-    
+
     /**
      * layout use function.
      *
@@ -186,7 +186,7 @@ class Quote
     public function isPlans()
     {
         $jsonArray = json_decode($this->ormObjFromLocal->plan_match_json, true);
-        
+
         $outArray = array();
 
         foreach ($jsonArray as $planArray) {
@@ -269,7 +269,7 @@ class Quote
 
     /**
      * Download the data from the source database, crontab use.
-     * 
+     *
      * @param string $t a = auto , m = not auto
      * @return array downloaded Quote ID
      */
@@ -278,13 +278,13 @@ class Quote
         $manyQuoteOrm = ORM::for_table('motor_quote', 'source')->
                     where('download', 0)->
                     order_by_asc('id');
-                    
+
         if ($t == 'a') {
             $manyQuoteOrm->where_lt('create_datetime', date("Y-m-d H:i:s", strtotime("-15 minutes")));
         }
-        
+
         $manyQuote = $manyQuoteOrm->find_many();
-        
+
         $arQuoteIdAr = array();
         foreach ($manyQuote as $quoteOrm) {
             $q = new Quote();
@@ -363,8 +363,10 @@ class Quote
             $this->dataToYellowSheet($sale);
             $this->haveAdData(); // if have ad data than insert to ad table
             $this->updateLocalPushStatus(1);
+            $this->updatePushStatusToKSI(1);
         } else {
             $this->updateLocalPushStatus(2); // 2 for Rubbish
+            $this->updatePushStatusToKSI(2);
         }
     }
 
@@ -377,6 +379,15 @@ class Quote
     {
         $this->ormObjFromLocal->status = $s;
         $this->ormObjFromLocal->save();
+    }
+
+    private function updatePushStatusToKSI($s)
+    {
+        $ksiOrm = ORM::for_table('sales_inte_online', 'ksi')->
+                where('id',  $this->ormObjFromLocal->id)->
+                find_one();
+        $ksiOrm->status = $s;
+        $ksiOrm->save();
     }
 
     /**
@@ -401,18 +412,18 @@ class Quote
         $yellowSheetOrm->Client_Address_3 = $this->emptyNullValue(mb_convert_encoding($this->ormObjFromLocal->address3, "BIG5", "UTF-8"));
         $yellowSheetOrm->Client_Address_5 = $this->emptyNullValue(mb_convert_encoding($this->ormObjFromLocal->address4, "BIG5", "UTF-8"));
         $yellowSheetOrm->Policy_Details = $this->ormObjFromLocal->insuranceType;
-        
+
         $yellowSheetOrm->Sum_Insured = $this->ormObjFromLocal->sum_insured;
-        
+
         $yellowSheetOrm->Make = $this->ormObjFromLocal->carMake;
         $yellowSheetOrm->Model =  mb_convert_encoding($this->ormObjFromLocal->carModel, "BIG5", "UTF-8");
         $yellowSheetOrm->Year_of_Manufacture = !empty($this->ormObjFromLocal->yearManufacture) ? $this->ormObjFromLocal->yearManufacture : 2000; // default value of the database;
         $yellowSheetOrm->Driver_One_Age = $this->ormObjFromLocal->age;
         $yellowSheetOrm->Driver_One_Driving_Experience = html_entity_decode($this->ormObjFromLocal->drivingExp);
         $yellowSheetOrm->Driver_One_Occupation = mb_convert_encoding(html_entity_decode($this->ormObjFromLocal->occupation), "BIG5", "UTF-8");
-        
+
         $yellowSheetOrm->NCD = $this->ormObjFromLocal->ncd;
-        
+
         $yellowSheetOrm->ksi_si_no = $this->ormObjFromLocal->ksi_si_no;
         $yellowSheetOrm->ys_client_no = $this->ormObjFromLocal->ys_client_no;
 
@@ -421,7 +432,7 @@ class Quote
         //print_r($planArray);
 
         $yellowSheetOrm->auto_premium  = !empty($planArray) ? 1 : 0 ;
-        
+
         if (isset($planArray[0])) {
             $yellowSheetOrm->Quote_One_NCD = $this->ormObjFromLocal->ncd;
             $yellowSheetOrm->Quote_One_Insurance_Type = $planArray[0]['TypeofInsurance'];
@@ -447,7 +458,7 @@ class Quote
             $yellowSheetOrm->Quote_One_Gross_Premium = $planArray[0]['gross'];
             $yellowSheetOrm->Additional_Plan = implode(',', $planArray[0]['subPlanName']);
         }
-        
+
         if (isset($planArray[1])) {
             $yellowSheetOrm->Quote_Two_NCD = $this->ormObjFromLocal->ncd;
             $yellowSheetOrm->Quote_Two_Insurance_Type = $planArray[1]['TypeofInsurance'];
@@ -472,7 +483,7 @@ class Quote
             $yellowSheetOrm->Quote_Two_MIB = $planArray[1]['mib'];
             $yellowSheetOrm->Quote_Two_Gross_Premium = $planArray[1]['gross'];
         }
-        
+
 
         $yellowSheetOrm->Start_Date = $this->dateFormate($this->ormObjFromLocal->policy_start_date);
         $yellowSheetOrm->Policy_End_Date = $this->dateFormate($this->ormObjFromLocal->policy_end_date);
@@ -483,10 +494,10 @@ class Quote
         $yellowSheetOrm->Client_Address_6 = $this->emptyNullValue(mb_convert_encoding($this->ormObjFromLocal->residential_district, "BIG5", "UTF-8"));//address line 5
         $yellowSheetOrm->Marital_Status = $this->ormObjFromLocal->marital_status;
         $yellowSheetOrm->DOB = $this->dateFormate($this->ormObjFromLocal->dob);
-        
+
         //$yellowSheetOrm->Vehicle_Registration = $this->ormObjFromLocal->vehicle_registration;
         $yellowSheetOrm->Vehicle_Registration = mb_convert_encoding($this->ormObjFromLocal->vehicle_registration, "BIG5", "UTF-8");
-        
+
         $yellowSheetOrm->Yearly_Mileage = $this->ormObjFromLocal->yearly_mileage;
         $yellowSheetOrm->Motor_Accident_Yrs = $this->ormObjFromLocal->motor_accident_yrs;
         $yellowSheetOrm->Drive_Offence_Point = $this->ormObjFromLocal->drive_offence_point;
@@ -521,7 +532,7 @@ class Quote
 
         $yellowSheetOrm->save();
     }
-    
+
     private function haveAdData()
     {
         if (
@@ -537,7 +548,7 @@ class Quote
         }
         return;
     }
-    
+
     private function adDataToYellowSheet()
     {
         $yellowSheetOrm = ORM::for_table('sales_inte_online_inquiries', 'ksi')->create();
@@ -550,7 +561,7 @@ class Quote
         $yellowSheetOrm->dvce = $this->ormObjFromLocal->dvce;
         $yellowSheetOrm->crtv = $this->ormObjFromLocal->crtv;
         $yellowSheetOrm->adps = $this->ormObjFromLocal->adps;
-        
+
         //echo($this->ormObjFromLocal->adps);
 
         $yellowSheetOrm->save();
@@ -636,12 +647,12 @@ class Quote
                                 '2' => array('value' => '0.00'),//TPPD U
                                 '3' => array('value' => '0.00'),//TPPD Y
                                 '4' => array('value' => '0.00'),//TPPD i
-                                '5' => array('value' => '0.00'),//General 
-                                '6' => array('value' => '0.00'),//Unnamed 
-                                '7' => array('value' => '0.00'),//Young 
-                                '8' => array('value' => '0.00'),//Inexp 
-                                '9' => array('value' => '0.00'),//Parking 
-                                '10' => array('value' => '0.00'),//Theft 
+                                '5' => array('value' => '0.00'),//General
+                                '6' => array('value' => '0.00'),//Unnamed
+                                '7' => array('value' => '0.00'),//Young
+                                '8' => array('value' => '0.00'),//Inexp
+                                '9' => array('value' => '0.00'),//Parking
+                                '10' => array('value' => '0.00'),//Theft
                             ),
                 'subPlanName' => array(),
                 'premium' => '0.00',
@@ -659,7 +670,7 @@ class Quote
     }
     // data formate process to yellow sheet end
 
-    
+
     private function typeOfInsuranceKeyToText($key)
     {
         $outArray['Third_Party_Only'] = 'Third Party';
