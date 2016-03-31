a096e41194f632004174727b4d50b40455c0b847
iLexN <ilex.job@gmail.com>
2016-03-31 16:57:50 +0800

    map age 1 / 88 / 99

--- model/QuoteLayout.php
+++ model/QuoteLayout.php
@@ -79,6 +79,24 @@ class QuoteLayout implements \ArrayAccess
     }
 
     /**
+     * map age range code.
+     * 
+     * @return string
+     */
+    public function showAge(){
+        switch ($this->ormObjFromLocal->age) {
+            case 1:
+                return '25-60';
+            case 88:
+                return '< 21';
+            case 99:
+                return '> 60';
+            default :
+                return $this->ormObjFromLocal->age;
+        }
+    }
+
+    /**
      * layout use function.
      *
      * @return string planName - SubPlaneName
